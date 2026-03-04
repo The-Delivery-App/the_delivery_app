706f932f41e7ab0e9304bfa5ff5f983e58a8bca5
rezorkin <ars.podshikal@gmail.com>
2026-03-04 22:24:22 +0000

    added food filters class to pass food filtering options

---
+++ the_delivery_app_flutter/lib/models/food_filters.dart
@@ -0,0 +1,11 @@
+class FoodFilter {
+  final double? priceTier;
+  final bool? discounted;
+  final String? category;
+  
+  const FoodFilter({
+    this.priceTier,
+    this.discounted,
+    this.category
+  });
+}
