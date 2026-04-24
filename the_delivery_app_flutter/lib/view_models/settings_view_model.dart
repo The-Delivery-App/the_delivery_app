34d487f7749017b16defe79e456293cde3c75e02
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-24 14:16:49 +0100

    setVM: class structure

--- the_delivery_app_flutter/lib/view_models/settings_view_model.dart
+++ the_delivery_app_flutter/lib/view_models/settings_view_model.dart
@@ -3,3 +3,13 @@ import 'package:flutter/foundation.dart';
 import '../models/settings.dart';
 import '../repositories/interfaces/i_settings_repository.dart';
 import '../state/settings_state.dart';
+
+class SettingsViewModel extends ChangeNotifier {
+  final ISettingsRepository _repository;
+  SettingsState _state = SettingsState(
+    settings: const Settings(language: 'en', notificationsEnabled: true),
+  );
+
+  SettingsViewModel({required ISettingsRepository repository})
+      : _repository = repository;
+}
