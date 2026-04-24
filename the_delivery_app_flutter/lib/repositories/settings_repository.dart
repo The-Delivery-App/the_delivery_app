36e0f8715a58fd55b4577711d1139da213369fab
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-24 14:11:28 +0100

    setRepo: class structure

--- the_delivery_app_flutter/lib/repositories/settings_repository.dart
+++ the_delivery_app_flutter/lib/repositories/settings_repository.dart
@@ -3,3 +3,10 @@ import 'dart:convert';
 import '../models/settings.dart';
 import '../storage/i_local_storage.dart';
 import 'interfaces/i_settings_repository.dart';
+
+class SettingsRepository implements ISettingsRepository {
+  final ILocalStorage _storage;
+  static const _key = 'settings';
+
+  SettingsRepository({required ILocalStorage storage}) : _storage = storage;
+}
