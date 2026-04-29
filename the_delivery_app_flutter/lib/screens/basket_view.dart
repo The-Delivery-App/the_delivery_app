8c12e7bee135cd23c9400f81139a0b729fca903c
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:23:34 +0100

    basketView: widget shell

--- the_delivery_app_flutter/lib/screens/basket_view.dart
+++ the_delivery_app_flutter/lib/screens/basket_view.dart
@@ -2,3 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../models/food.dart';
 import '../view_models/basket_view_model.dart';
+
+class BasketView extends StatelessWidget {
+  final BasketViewModel viewModel;
+
+  const BasketView({super.key, required this.viewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
