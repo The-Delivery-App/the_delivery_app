ff93cf9efe57973ceaa6ce73a0977dff276e5a02
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:21:45 +0100

    mainView: bottom nav bar

--- the_delivery_app_flutter/lib/screens/main_view.dart
+++ the_delivery_app_flutter/lib/screens/main_view.dart
@@ -37,6 +37,19 @@ class _MainViewState extends State<MainView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _buildBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTabTapped,
+        selectedItemColor: Colors.deepOrange,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Feed'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_basket), label: 'Basket'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+        ],
+      ),
     );
   }
 }
