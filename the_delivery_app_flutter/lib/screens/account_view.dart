97fe8987679f88d144abc6b08d73330ab73de922
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:35:58 +0100

    accountView: widget shell

--- the_delivery_app_flutter/lib/screens/account_view.dart
+++ the_delivery_app_flutter/lib/screens/account_view.dart
@@ -2,3 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
 
 import '../main.dart';
+
+class AccountView extends StatefulWidget {
+  const AccountView({super.key});
+
+  @override
+  State<AccountView> createState() => _AccountViewState();
+}
+
+class _AccountViewState extends State<AccountView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
