5686e766e27ba9a9d39b81900699ecfdb02b0076
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:39:30 +0100

    widget shell

--- the_delivery_app_flutter/lib/screens/order_status_view.dart
+++ the_delivery_app_flutter/lib/screens/order_status_view.dart
@@ -2,3 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../models/order_status.dart';
 import '../state/order_status_state.dart';
+
+class OrderStatusView extends StatelessWidget {
+  final OrderStatusState state;
+
+  const OrderStatusView({super.key, required this.state});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
