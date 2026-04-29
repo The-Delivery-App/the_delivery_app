be950b494b324b51eb008a2fdeabea8035afba64
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:36:22 +0100

    accountView: auth setup

--- the_delivery_app_flutter/lib/screens/account_view.dart
+++ the_delivery_app_flutter/lib/screens/account_view.dart
@@ -11,6 +11,27 @@ class AccountView extends StatefulWidget {
 }
 
 class _AccountViewState extends State<AccountView> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    client.auth.authInfoListenable.addListener(_updateSignedInState);
+    _isSignedIn = client.auth.isAuthenticated;
+  }
+
+  @override
+  void dispose() {
+    client.auth.authInfoListenable.removeListener(_updateSignedInState);
+    super.dispose();
+  }
+
+  void _updateSignedInState() {
+    setState(() {
+      _isSignedIn = client.auth.isAuthenticated;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
