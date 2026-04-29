adf9ae272306069f5fe78561e9db6a2b5d1e5c46
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:15:40 +0100

    searchVM: class structure

--- the_delivery_app_flutter/lib/view_models/search_view_model.dart
+++ the_delivery_app_flutter/lib/view_models/search_view_model.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/foundation.dart';
 
 import '../state/search_state.dart';
+
+class SearchViewModel extends ChangeNotifier {
+  SearchState _state = const SearchState(
+    query: '',
+    results: [],
+    isLoading: false,
+  );
+
+  SearchViewModel();
+}
