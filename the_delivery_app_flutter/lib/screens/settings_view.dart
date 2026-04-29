39d710e9bac9e9a4a270f236eec7061ea1bd4c0b
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:26:31 +0100

    setView: widget shell

--- the_delivery_app_flutter/lib/screens/settings_view.dart
+++ the_delivery_app_flutter/lib/screens/settings_view.dart
@@ -2,3 +2,19 @@ import 'package:flutter/material.dart';
 
 import '../models/settings.dart';
 import '../view_models/settings_view_model.dart';
+
+class SettingsView extends StatefulWidget {
+  final SettingsViewModel viewModel;
+
+  const SettingsView({super.key, required this.viewModel});
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
