58be56a57be2836ab29d87c33686a377eba32ea0
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:31:50 +0100

    restView: rest header

--- the_delivery_app_flutter/lib/screens/restaurant_view.dart
+++ the_delivery_app_flutter/lib/screens/restaurant_view.dart
@@ -13,6 +13,22 @@ class RestaurantView extends StatelessWidget {
     required this.menuItems,
   });
 
+  Widget _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(restaurant.name,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 4),
+          Text('ID: ${restaurant.id}',
+              style: const TextStyle(fontSize: 12, color: Colors.grey)),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
