5151c7357a01286fc57b83e9739ee787770ad4fb
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:45:34 +0100

    mapView: widget shell

--- the_delivery_app_flutter/lib/screens/map_view.dart
+++ the_delivery_app_flutter/lib/screens/map_view.dart
@@ -2,3 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../models/restaurant.dart';
 import '../state/map_state.dart';
+
+class MapView extends StatelessWidget {
+  final MapState state;
+
+  const MapView({super.key, required this.state});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
