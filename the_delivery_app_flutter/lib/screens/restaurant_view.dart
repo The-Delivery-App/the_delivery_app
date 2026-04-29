99007571717d2f43ca97b9e867e59a1febe47b0d
k1ngofaynt <yusufalfahad72@gmail.com>
2026-04-29 01:31:11 +0100

    restView: widget shell

--- the_delivery_app_flutter/lib/screens/restaurant_view.dart
+++ the_delivery_app_flutter/lib/screens/restaurant_view.dart
@@ -2,3 +2,19 @@ import 'package:flutter/material.dart';
 
 import '../models/food.dart';
 import '../models/restaurant.dart';
+
+class RestaurantView extends StatelessWidget {
+  final Restaurant restaurant;
+  final List<Food> menuItems;
+
+  const RestaurantView({
+    super.key,
+    required this.restaurant,
+    required this.menuItems,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
