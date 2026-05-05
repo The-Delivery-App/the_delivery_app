5b45968caff9a5a6170272dc88dcf2c55a24ca90
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 16:28:45 +0100

    filterVM: class structure

--- the_delivery_app_flutter/lib/view_models/filter_view_model.dart
+++ the_delivery_app_flutter/lib/view_models/filter_view_model.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/foundation.dart';
 
 import '../state/filter_state.dart';
+
+class FilterViewModel extends ChangeNotifier {
+  FilterState _state = const FilterState(
+    foodFilters: [],
+    restaurantFilters: [],
+  );
+
+  FilterViewModel();
+
+  FilterState getState() => _state;
+}
