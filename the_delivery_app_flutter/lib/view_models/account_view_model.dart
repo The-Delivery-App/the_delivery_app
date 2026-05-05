d063f342c7919de583d932b9fc02dd7ae1b245e9
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 16:31:23 +0100

    accountVM: class structure

--- the_delivery_app_flutter/lib/view_models/account_view_model.dart
+++ the_delivery_app_flutter/lib/view_models/account_view_model.dart
@@ -3,3 +3,11 @@ import 'package:flutter/foundation.dart';
 import '../models/session.dart';
 import '../models/user.dart';
 import '../state/account_state.dart';
+
+class AccountViewModel extends ChangeNotifier {
+  AccountState _state = const AccountState(isLoggedIn: false);
+
+  AccountViewModel();
+
+  AccountState getState() => _state;
+}
