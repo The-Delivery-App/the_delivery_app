5f998abc846f2d1fbe04d27bbea8b0292f47ff7e
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 21:46:30 +0100

    import restaurant view in map view

--- the_delivery_app_flutter/lib/screens/map_view.dart
+++ the_delivery_app_flutter/lib/screens/map_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../models/restaurant.dart';
 import '../state/map_state.dart';
+import 'restaurant_view.dart';
 
 class MapView extends StatelessWidget {
   final MapState state;
