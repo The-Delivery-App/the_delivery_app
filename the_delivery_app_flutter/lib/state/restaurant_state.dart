af801aef8397c1da26809e372e69add50db3ee70
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 21:17:04 +0100

    improve restaurant state

--- the_delivery_app_flutter/lib/state/restaurant_state.dart
+++ the_delivery_app_flutter/lib/state/restaurant_state.dart
@@ -4,10 +4,14 @@ class RestaurantState {
   final List<Food> menuItems;
   final bool isLoading;
   final String? errorMessage;
+  final String? cuisine;
+  final int? deliveryMinutes;
 
   const RestaurantState({
     required this.menuItems,
     required this.isLoading,
     this.errorMessage,
+    this.cuisine,
+    this.deliveryMinutes,
   });
 }
