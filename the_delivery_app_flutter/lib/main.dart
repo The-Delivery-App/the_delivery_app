206c62ffd57a3f00a61c8bf04e87ca84101ff83b
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 18:18:15 +0100

    rename app title

--- the_delivery_app_flutter/lib/main.dart
+++ the_delivery_app_flutter/lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Serverpod Demo',
+      title: 'The Delivery App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const MainView(),
     );
