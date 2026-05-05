f9865a932261f3bca9ec6aa797bc1909eaf622f4
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 20:46:57 +0100

    change app theme colour

--- the_delivery_app_flutter/lib/main.dart
+++ the_delivery_app_flutter/lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'The Delivery App',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(colorSchemeSeed: Colors.deepOrange),
       home: const MainView(),
     );
   }
