be0e0cfe104f1f433a071326f362def28362acea
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 17:44:16 +0100

    feedState: add errorMessage field

--- the_delivery_app_flutter/lib/state/feed_state.dart
+++ the_delivery_app_flutter/lib/state/feed_state.dart
@@ -3,9 +3,11 @@ import '../models/food.dart';
 class FeedState {
   final List<Food> feedItems;
   final bool isLoading;
+  final String? errorMessage;
 
   const FeedState({
     required this.feedItems,
     required this.isLoading,
+    this.errorMessage,
   });
 }
