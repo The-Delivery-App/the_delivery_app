1c386859d9451b15b8e880a2e37e7b9f9e3b4ab5
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 18:29:03 +0100

    add errorMessage to RestaurantState

--- the_delivery_app_flutter/lib/state/restaurant_state.dart
+++ the_delivery_app_flutter/lib/state/restaurant_state.dart
@@ -3,9 +3,11 @@ import '../models/food.dart';
 class RestaurantState {
   final List<Food> menuItems;
   final bool isLoading;
+  final String? errorMessage;
 
   const RestaurantState({
     required this.menuItems,
     required this.isLoading,
+    this.errorMessage,
   });
 }
