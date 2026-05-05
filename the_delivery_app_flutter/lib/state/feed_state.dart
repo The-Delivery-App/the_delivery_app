78763b8a23e96d1e3d1e9b99aa8ace4237f36bbf
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-05 20:32:09 +0100

    add isLoadingMore to feed state

--- the_delivery_app_flutter/lib/state/feed_state.dart
+++ the_delivery_app_flutter/lib/state/feed_state.dart
@@ -4,10 +4,12 @@ class FeedState {
   final List<Food> feedItems;
   final bool isLoading;
   final String? errorMessage;
+  final bool isLoadingMore;
 
   const FeedState({
     required this.feedItems,
     required this.isLoading,
     this.errorMessage,
+    this.isLoadingMore = false,
   });
 }
