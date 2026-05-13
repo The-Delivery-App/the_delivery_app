aaf6784e1f9ae4828c3352a90b3906dd406a4253
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-13 17:49:30 +0100

    add border to near you card

--- the_delivery_app_flutter/lib/screens/feed_view.dart
+++ the_delivery_app_flutter/lib/screens/feed_view.dart
@@ -120,7 +120,8 @@ class FeedView extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(14),
-          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 4, offset: const Offset(0, 2))],
+          border: Border.all(color: Colors.grey.shade200),
+          boxShadow: [BoxShadow(color: Colors.black.withValues(alpha: 0.04), blurRadius: 6, offset: const Offset(0, 2))],
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
