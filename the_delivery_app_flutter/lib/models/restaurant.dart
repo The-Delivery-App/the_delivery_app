c6fef8de84ffd16f44c25518a2f1d3742fbda6e8
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-15 18:43:50 +0100

    add lat lng to restaurant model

--- the_delivery_app_flutter/lib/models/restaurant.dart
+++ the_delivery_app_flutter/lib/models/restaurant.dart
@@ -1,6 +1,8 @@
 class Restaurant {
   final String id;
   final String name;
+  final double? latitude;
+  final double? longitude;
 
-  const Restaurant({required this.id, required this.name});
+  const Restaurant({required this.id, required this.name, this.latitude, this.longitude});
 }
