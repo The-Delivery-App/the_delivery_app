3377e317297429ec008d5c5e688ab5aaed3bfa9c
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-15 19:15:36 +0100

    create checkout view file

---
+++ the_delivery_app_flutter/lib/screens/checkout_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../view_models/basket_view_model.dart';
+
+class CheckoutView extends StatefulWidget {
+  final BasketViewModel basketViewModel;
+
+  const CheckoutView({super.key, required this.basketViewModel});
+
+  @override
+  State<CheckoutView> createState() => _CheckoutViewState();
+}
+
+class _CheckoutViewState extends State<CheckoutView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAF7F2),
+      appBar: AppBar(
+        title: const Text('Checkout'),
+        backgroundColor: const Color(0xFFFAF7F2),
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: const SizedBox.shrink(),
+    );
+  }
+}
