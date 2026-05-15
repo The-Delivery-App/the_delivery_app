5ad8dc186e7ac2283acd033fb7ec3b6ed26059db
k1ngofaynt <yusufalfahad72@gmail.com>
2026-05-15 19:35:02 +0100

    add fetch addresses method

--- the_delivery_app_flutter/lib/screens/addresses_view.dart
+++ the_delivery_app_flutter/lib/screens/addresses_view.dart
@@ -16,6 +16,28 @@ class _AddressesViewState extends State<AddressesView> {
   bool _isLoading = true;
 
   @override
+  void initState() {
+    super.initState();
+    _loadAddresses();
+  }
+
+  Future<void> _loadAddresses() async {
+    setState(() => _isLoading = true);
+    final userRaw = await client.userProfileController.getCurrentUser();
+    final userId = jsonDecode(userRaw)['userId'] as int;
+    final profileRaw = await client.userProfileController.getProfile(userId);
+    final profile = jsonDecode(profileRaw);
+    final list = (profile['user']['addresses'] as List<dynamic>)
+        .whereType<Map<String, dynamic>>()
+        .toList();
+    if (!mounted) return;
+    setState(() {
+      _addresses = list;
+      _isLoading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFAF7F2),
