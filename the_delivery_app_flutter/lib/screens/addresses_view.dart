a4f6e2402aedafce49ca09264318ea8e84049b53
rezorkin <ars.podshikal@gmail.com>
2026-05-26 16:01:08 +0100

    Updated client and addresses to correctly save active address.

--- the_delivery_app_flutter/lib/screens/addresses_view.dart
+++ the_delivery_app_flutter/lib/screens/addresses_view.dart
@@ -115,11 +115,7 @@ class _AddressesViewState extends State<AddressesView> {
   }
 
   Future<void> _setDefault(int addressId) async {
-    await http.post(
-      Uri.parse('http://localhost:8080/userProfileController/setDefaultAddress'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'addressId': addressId}),
-    );
+    await client.userProfileController.setDefaultAddress(addressId);
     _loadAddresses();
   }
 
